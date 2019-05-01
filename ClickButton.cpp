6871e071dd9a97b41a7451115ff353120791b8b0
akarnaukh <a.karnauh@gmail.com>
2019-05-01 15:10:48 +0300

    Update ClickButton.cpp

--- ClickButton.cpp
+++ ClickButton.cpp
@@ -1,60 +1,3 @@
-/*    ClickButton
- 
- Arduino library that decodes multiple clicks on one button.
- Also copes with long clicks and click-and-hold.
- 
- Usage: ClickButton buttonObject(pin [LOW/HIGH, [CLICKBTN_PULLUP]]);
- 
-  where LOW/HIGH denotes active LOW or HIGH button (default is LOW)
-  CLICKBTN_PULLUP is only possible with active low buttons.
- 
-
- Returned click counts:
-
-   A positive number denotes the number of (short) clicks after a released button
-   A negative number denotes the number of "long" clicks
- 
-NOTE!
- This is the OPPOSITE/negative of click codes from the last pre-2013 versions!
- (this seemed more logical and simpler, so I finally changed it)
-
- Based on the Debounce example at arduino playground site
-
- 
- Copyright (C) 2010,2012, 2013 raron
-
-  
- GNU GPLv3 license
- 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
- 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- 
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
-
- Contact: raronzen@gmail.com
-
- 
- History:
- 2013.08.29 - Some small clean-up of code, more sensible variable names etc.
-                Added another example code for multiple buttons in an object array
- 2013.04.23 - A "minor" debugging: active-high buttons now work (wops)!
-                Thanks goes to John F. H. for pointing that out!
- 2013.02.17 - Some improvements, simplified click codes.
-				Added a LED fader example. Thanks to Tom K. for the idea.
- 2012.01.31 - Tiny update for Arduino 1.0
- 2010.06.15 - First version. Basically just a small OOP programming exercise.
-*/
-
 #include "ClickButton.h"
 
 ClickButton::ClickButton(uint8_t buttonPin)
@@ -65,6 +8,7 @@ ClickButton::ClickButton(uint8_t buttonPin)
   _lastState     = _btnState;
   _clickCount    = 0;
   clicks         = 0;
+  longs          = false;
   depressed      = false;
   _lastBounceTime= 0;
   debounceTime   = 20;            // Debounce timer in ms
@@ -83,6 +27,7 @@ ClickButton::ClickButton(uint8_t buttonPin, boolean activeType)
   _lastState     = _btnState;
   _clickCount    = 0;
   clicks         = 0;
+  longs          = false;
   depressed      = 0;
   _lastBounceTime= 0;
   debounceTime   = 20;            // Debounce timer in ms
@@ -100,6 +45,7 @@ ClickButton::ClickButton(uint8_t buttonPin, boolean activeType, boolean internal
   _lastState     = _btnState;
   _clickCount    = 0;
   clicks         = 0;
+  longs          = false;
   depressed      = 0;
   _lastBounceTime= 0;
   debounceTime   = 20;            // Debounce timer in ms
@@ -116,6 +62,7 @@ ClickButton::ClickButton(uint8_t buttonPin, boolean activeType, boolean internal
 void ClickButton::Update()
 {
   long now = (long)millis();      // get current time
+  longs = false;
   _btnState = digitalRead(_pin);  // current appearant button state
 
   // Make the button logic active-high in code
@@ -126,10 +73,19 @@ void ClickButton::Update()
 
 
   // debounce the button (Check if a stable, changed state has occured)
-  if (now - _lastBounceTime > debounceTime && _btnState != depressed)
+
+  if (now - _lastBounceTime > longClickTime && _btnState)
+  {
+    _lastBounceTime = now;
+    depressed = _btnState;
+    if (depressed) _clickCount--;
+    longs = true;
+    clicks = _clickCount;
+  } else if (now - _lastBounceTime > debounceTime && _btnState != depressed)
   {
     depressed = _btnState;
     if (depressed) _clickCount++;
+    clicks = _clickCount;
   }
 
   if(_lastState == _btnState) changed = false;
@@ -143,14 +99,4 @@ void ClickButton::Update()
     _clickCount = 0;
     if(clicks != 0) changed = true;
   }
-
-  // Check for "long click"
-  if (depressed && (now - _lastBounceTime > longClickTime))
-  {
-    // negative count for long clicks
-    clicks = 0 - _clickCount;
-    _clickCount = 0;
-    if(clicks != 0) changed = true;
-  }
 }
-
